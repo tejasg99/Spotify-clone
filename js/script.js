dccc6851e6d9d8d40999faeea4d76d4ec24b6eeb
Tejas Gawade <tgawade092@gmail.com>
2024-01-08 12:41:50 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function SecondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder){
     currFolder = folder;
-    let a =await fetch(`./${folder}/`)
+    let a =await fetch(`https://tejasg99.github.io/Spotify-clone/songs/Joyous-Hits/${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
